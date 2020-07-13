070583ad76672c424193332f7c055dc288f7005a
Jeremy Tsai <jeremyt1114@g.ucla.edu>
2020-07-14 02:12:21 +0800

    Added credential-fetching functionality.

--- email-notif-bot.py
+++ email-notif-bot.py
@@ -1,10 +1,30 @@
 from datetime import datetime
-import sys, json
+import imaplib
+import sys, json, os
+
+#load login credentials from credentials.json
+def load_credentials():
+    filepath = os.path.join(os.getcwd(), './credentials.json')
+    sys.stdout.write("Looking for login credentials at " + filepath + "......")
+    try:
+        with open(filepath) as f:
+            data = json.load(f)
+            if 'username' in data.keys() and 'password' in data.keys():
+                username = data['username']
+                password = data['password']
+            else:
+                raise Exception("Cannot find object with attribute 'username' and/or 'password' in JSON. Check credentials.json.")
+            sys.stdout.write(' success.\nLogging in as ' + username + '.\n')
+    except Exception as e:
+        sys.stdout.write(' failed.\n'+ str(e)+'\n')
+        sys.stdout.write('Script execution aborted.\n')
+        exit()
 
 def main():
     #log entry prefix
     now = datetime.now()
     sys.stdout.write("\nNEW RUN STARTING ON " + str(now) + '\n')
+    load_credentials()
     while True:
         sys.stdout.flush()
 
